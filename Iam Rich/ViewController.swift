8fb48fd0a9eeb687261e801d3e12155568e0f910
dandyferdiansyah <68523782+muhammadferdiansyah@users.noreply.github.com>
2020-12-09 09:20:36 +0700

    Add files via upload

---
+++ Iam Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Iam Rich
+//
+//  Created by Dandy Ferdiansyah on 09/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
